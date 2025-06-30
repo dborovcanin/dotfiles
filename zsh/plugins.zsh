452ff417657577b764b4c25671f31a1f059fc81e
Dusan Borovcanin <borovcanindusan1@gmail.com>
2025-06-30 07:55:28 +0200

    Update Picom and i3 setup
    Signed-off-by: Dusan Borovcanin <borovcanindusan1@gmail.com>

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -18,14 +18,10 @@ bindkey -M vicmd v edit-command-line
 
 __fif__() {
 	RG_PREFIX="rga --files-with-matches"
-	local file
-	file="$(
-		FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'" \
-			fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
-				--phony -q "$1" \
-				--bind "change:reload:$RG_PREFIX {q}" \
-				--preview-window="75%:wrap"
-	)" &&
-	echo "opening $file" &&
-	xdg-open "$file"
+	FZF_DEFAULT_COMMAND="$RG_PREFIX '$1'"
+		fzf --sort --preview="[[ ! -z {} ]] && rga --pretty --context 5 {q} {}" \
+			--phony -q "$1" \
+			--bind "change:reload:$RG_PREFIX {q}" \
+			--preview-window="75%:wrap" \
+	| xargs $EDITOR
 }
